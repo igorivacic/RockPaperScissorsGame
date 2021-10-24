41e5700d084ff253f0b4b7dbdfdd637c26469166
Igor Ivacic <igor.ivacic.2740@gmail.com>
2021-10-24 21:35:03 +0200

    Add routes and RockPaperScissorsController.

---
+++ app/controllers/games/rock_paper_scissors_controller.rb
@@ -0,0 +1,43 @@
+require 'games/rock_paper_scissors_game'
+
+module Games
+  class RockPaperScissorsController < ApplicationController
+    def new; end
+
+    def create
+      game = Games::RockPaperScissors.new
+      game.player_1 = game_params[:player].present? ? game_params[:player] : Games::RockPaperScissorsGame.find_random_name
+      game.player_2 = Games::RockPaperScissorsGame.find_random_name
+      game.moves = init_game_moves(game_params[:move])
+
+      if game.save
+        redirect_to games_rock_paper_scissor_path(game)
+      else
+        render :new
+      end
+    end
+
+    def show
+      @game = Games::RockPaperScissors.find(params[:id])
+    end
+
+    def index
+      @games = Games::RockPaperScissors.all
+    end
+
+    private
+
+      def game_params
+        params.permit(:player, :move)
+      end
+
+      def init_game_moves(move)
+        [
+          {
+            player_1: move.present? ? move : Games::RockPaperScissorsGame.find_random_move,
+            player_2: Games::RockPaperScissorsGame.find_random_move
+          }
+        ]
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #
-  root to: 'pages#home'
+  root to: 'games/rock_paper_scissors#new'
+
+  namespace :games do
+    resources :rock_paper_scissors
+  end
 end
