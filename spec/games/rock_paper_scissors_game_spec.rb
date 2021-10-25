d7a70c109a896ebc10de83cda44f85643b14449f
Igor Ivacic <igor.ivacic.2740@gmail.com>
2021-10-25 13:17:23 +0200

    Add controller and model specs and fixed some falling specs.

--- spec/games/rock_paper_scissors_game_spec.rb
+++ spec/games/rock_paper_scissors_game_spec.rb
@@ -1,32 +1,31 @@
-require 'games/rock_paper_scissors'
+require 'games/rock_paper_scissors_game'
 require 'rails_helper'
 
 describe Games::RockPaperScissorsGame do
-
   describe 'run' do
     context 'with bad arguments' do
       it 'raises exception when player_1 is nil' do
-        expect {
+        expect do
           described_class.new(player_1: nil, player_2: 'Rock')
-        }.to raise_exception Games::RockPaperScissorsGame::Error
+        end.to raise_exception Games::RockPaperScissorsGame::Error
       end
 
       it 'raises exception when player_2 is nil' do
-        expect {
+        expect do
           described_class.new(player_1: 'Rock', player_2: nil)
-        }.to raise_exception Games::RockPaperScissorsGame::Error
+        end.to raise_exception Games::RockPaperScissorsGame::Error
       end
 
       it 'raises exception when player_1 is not Rock, Paper or Scissors' do
-        expect {
+        expect do
           described_class.new(player_1: 'Rocky', player_2: 'Balboa')
-        }.to raise_exception Games::RockPaperScissorsGame::Error
+        end.to raise_exception Games::RockPaperScissorsGame::Error
       end
 
       it 'raises exception when player_2 is not Rock, Paper or Scissors' do
-        expect {
+        expect do
           described_class.new(player_1: 'Lizzard', player_2: 'Eagle')
-        }.to raise_exception Games::RockPaperScissorsGame::Error
+        end.to raise_exception Games::RockPaperScissorsGame::Error
       end
     end
 
