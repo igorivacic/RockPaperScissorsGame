d7e5bc99da1505d5135e54a2d8e20e32aaac7033
Igor Ivacic <igor.ivacic.2740@gmail.com>
2021-10-25 23:19:11 +0200

    Rubocop fixes.

--- app/controllers/games/rock_paper_scissors_controller.rb
+++ app/controllers/games/rock_paper_scissors_controller.rb
@@ -6,7 +6,8 @@ module Games
 
     def create
       game = Games::RockPaperScissors.new
-      game.player_1 = game_params[:player].present? ? game_params[:player] : Games::RockPaperScissorsGame.find_random_name
+      game.player_1 = game_params[:player].present? ?
+                        game_params[:player] : Games::RockPaperScissorsGame.find_random_name
       game.player_2 = Games::RockPaperScissorsGame.find_random_name
       game.moves = init_game_moves(game_params[:move])
 
@@ -27,9 +28,9 @@ module Games
 
     private
 
-      def game_params
-        params.permit(:player, :move)
-      end
+    def game_params
+      params.permit(:player, :move)
+    end
 
       def init_game_moves(move)
         [
